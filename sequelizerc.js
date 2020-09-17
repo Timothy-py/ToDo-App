aa281dc704475fb5aa9e72392bb65668f605ead6
Timothy-py <adeyeyetimothy33@gmail.com>
2020-09-17 22:41:36 +0100

    added comment to sequelizerc.js file

--- sequelizerc.js
+++ sequelizerc.js
@@ -4,8 +4,8 @@ const path = require("path");
 
 
 module.exports = {
-    "config": path.resolve('./server/config', 'config.json'),
-    "models-path": path.resolve('./server/models'),
-    "seeders-path": path.resolve('./server/seeders'),
-    "migrations-path": path.resolve('./server/migrations')
+    "config": path.resolve('./server/config', 'config.json'), //contains app config settings e.g DB auth config
+    "models-path": path.resolve('./server/models'), // holds the application models
+    "seeders-path": path.resolve('./server/seeders'), // holds seed data
+    "migrations-path": path.resolve('./server/migrations') // contains app migrations
 };
