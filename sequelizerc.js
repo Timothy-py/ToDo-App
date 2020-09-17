7b09962358712d4c421e3d56d67106c555a3ce54
Timothy-py <adeyeyetimothy33@gmail.com>
2020-09-17 22:55:38 +0100

    initialized sequelize commamd and autogenerated needed files and folders

--- sequelizerc.js
+++ sequelizerc.js
@@ -4,8 +4,8 @@ const path = require("path");
 
 
 module.exports = {
-    "config": path.resolve('./server/config', 'config.json'), //contains app config settings e.g DB auth config
-    "models-path": path.resolve('./server/models'), // holds the application models
-    "seeders-path": path.resolve('./server/seeders'), // holds seed data
-    "migrations-path": path.resolve('./server/migrations') // contains app migrations
+    "config": path.resolve('./dbServer/config', 'config.json'), //contains app config settings e.g DB auth config
+    "models-path": path.resolve('./dbServer/models'), // holds the application models
+    "seeders-path": path.resolve('./dbServer/seeders'), // holds seed data
+    "migrations-path": path.resolve('./dbServer/migrations') // contains app migrations
 };
